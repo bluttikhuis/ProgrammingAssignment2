88e29fd61c7fc344bf762242cd4413595208daf3
Berry Luttikhuis <bluttikhuis@gmail.com>
2015-04-23 12:24:21 +0200

    Completed Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function, 'makeCacheMatrix', stores the input matrix as well as its cached inverse,
+## which is or is not created by 'cacheSolve'. 'cacheSolve' is a function who checks if 'makeCacheMatrix' 
+## did store the inverse matrix and, if it didn't, creates the inverse of the original matrix (which
+## was put in as an argument for 'makeCacheMatrix').
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cacheimat <- NULL
+  set <- function(y = matrix()) {
+    x <<- y
+    cacheimat <<- NULL
+  }
+  get <- function() x
+  setimat <- function(imat) cacheimat <<- imat
+  getimat <- function() cacheimat
+  list(set = set, get = get, setimat = setimat, getimat = getimat)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  imat <- x$getimat()
+  if(!is.null(imat)) {
+    message("getting cached data")
+    return(imat)
+  }
+  getx <- x$get()
+  imat <- solve(getx, ...)
+  x$setimat(imat)    
+  imat
 }
